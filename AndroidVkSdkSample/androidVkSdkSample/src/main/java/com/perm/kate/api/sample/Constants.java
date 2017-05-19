fdc35b5ffcecb82584971eff5befbe50830f1cae
chugaev <chugaev.tolya@gmail.com>
2017-05-19 13:10:50 +0300

    AndroidVkSdkSample

--- AndroidVkSdkSample/androidVkSdkSample/src/main/java/com/perm/kate/api/sample/Constants.java
+++
@@ -1,5 +0,0 @@
-package com.perm.kate.api.sample;
-
-public class Constants {
-    public static String API_ID="5673342";
-}
--- AndroidVkSdkSample/androidVkSdkSample/src/main/java/com/perm/kate/api/sample/ListTest.java
+++
@@ -1,8 +0,0 @@
-package com.perm.kate.api.sample;
-
-/**
- * Created by lenka on 10.04.17.
- */
-
-public class ListTest extends ListFragment {
-}
